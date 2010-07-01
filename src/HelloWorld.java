ab58fbc2b814da152ef88031ef278ceeb9acec74
nerkis <nerkis@bowdoin.edu>
2010-07-01 17:17:38 -0400

    Message changed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 
 	public static void main (String[] args) {
 	
-		System.out.println("Hello brave new exciting frustrating and maybe working GIT world!!");
+		System.out.println("!!");
 	}
 }
