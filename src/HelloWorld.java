02e72150d99149186ce34a3ccbfc4f79ce6327da
majercik <smajerci@bowdoin.edu>
2010-07-01 16:07:38 -0400

    sfkalsdfjs;d
    Signed-off-by: majercik <smajerci@bowdoin.edu>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 
 	public static void main (String[] args) {
 	
-		System.out.println("Hello brave new exciting GIT world!!");
+		System.out.println("Hello brave new exciting frustrating GIT world!!");
 	}
 	
 }
