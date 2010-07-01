cdb848db003f9ad9436a74ae727d3983cd4f7bfd
smajerci <smajerci@139.140.110.76>
2010-07-01 14:04:00 -0400

    Try try again.
    Signed-off-by: smajerci <smajerci@139.140.110.76>

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 
 	public static void main (String[] args) {
 	
-		System.out.println("Hello brave new world!!");
+		System.out.println("Hello brave new GIT world!!");
 	}
 	
 }
