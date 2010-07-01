f774e7400bbe546c49b10ed04ba96804a615954b
nerkis <nerkis@bowdoin.edu>
2010-07-01 17:06:41 -0400

    Merged versions

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,4 @@
 
-<<<<<<< HEAD
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-=======
-
-
 
 public class HelloWorld {
 
@@ -15,6 +6,4 @@ public class HelloWorld {
 	
 		System.out.println("Hello brave new exciting frustrating and maybe working GIT world!!");
 	}
-	
->>>>>>> 718d7a53d5857a1ff30ab06462afe3840251031a
 }
